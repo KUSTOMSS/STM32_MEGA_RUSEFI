0a8168298ab2aa7edf03834ddb05486d6bfaed02
GitHub git update Action <action@github.com>
2024-12-12 00:50:19 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_STM32_MEGA.h
+++ generated/controllers/generated/rusefi_generated_STM32_MEGA.h
@@ -1797,7 +1797,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.12.11.STM32_MEGA.3433422265"
+#define TS_SIGNATURE "rusEFI main.2024.12.12.STM32_MEGA.3433422265"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
