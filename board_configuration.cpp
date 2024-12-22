49b90bdebec7113695de6c34136739c216f17c21
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2024-12-22 15:57:46 +0100

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -19,6 +19,8 @@ void setBoardDefaultConfiguration() {
 	// SPI1 AUX
 	engineConfiguration->is_enabled_spi_1 = true;
 	engineConfiguration->max31855spiDevice = SPI_DEVICE_3;
+	engineConfiguration->max31855spiDevice = SPI_DEVICE_2;
+	engineConfiguration->max31855spiDevice = SPI_DEVICE_1;
 	engineConfiguration->spi1mosiPin = Gpio::B4;
 	engineConfiguration->spi1misoPin = Gpio::B5;
 	engineConfiguration->spi1sckPin = Gpio::B3;
@@ -37,6 +39,12 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->crankingInjectionMode = IM_SEQUENTIAL;
 	engineConfiguration->injectionMode = IM_SEQUENTIAL;
 
+	//CAN bus overwrites
+        engineConfiguration->canWriteEnabled = true;
+	engineConfiguration->canReadEnabled = true;
+	engineConfiguration->canSleepPeriodMs = 50;
+	engineConfiguration->canBaudRate = B500KBPS;
+	
 	//Bias resistor
 	engineConfiguration->clt.config.bias_resistor = 2490;
 	engineConfiguration->iat.config.bias_resistor = 2490;
