8e620a676d3b7a738b45d54ec46387c830548c55
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2024-12-23 16:41:07 +0100

    Update board.mk

--- board.mk
+++ board.mk
@@ -9,6 +9,7 @@ include $(BOARD_DIR)/meta-info.env
 
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
+DDEFS += -DEFI_MAX_31855=TRUE
 
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
