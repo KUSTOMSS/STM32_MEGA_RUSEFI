0da2155a2bbfbc440e5078d799eccd72da6f846b
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2024-12-23 16:42:44 +0100

    Update board.mk

--- board.mk
+++ board.mk
@@ -9,7 +9,10 @@ include $(BOARD_DIR)/meta-info.env
 
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
+
+DDEFS += -DSTM32F407xx
 DDEFS += -DEFI_MAX_31855=TRUE
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
 
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
