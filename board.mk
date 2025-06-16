1f0bfd269072c86aa8e9cea73240cc28573f06a8
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2025-06-16 10:21:28 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -6,6 +6,13 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=100
+
+DDEFS += -DSTM32F407xx
+DDEFS += -DEFI_MAX_31855=TRUE
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
+
 # this would save some flash while being unable to update WBO controller firmware
 DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
 
