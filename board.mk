38bfbfd127b6a834b7b62c8f54bb6783d38ec010
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2025-06-16 12:37:11 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,16 +1,19 @@
-# List of all the board related files.
-BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
+    $(BOARD_DIR)/default_tune.cpp \
+
+
+BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
-# Override DEFAULT_ENGINE_TYPE
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=100
+
 DDEFS += -DSTM32F407xx
 DDEFS += -DEFI_MAX_31855=TRUE
 DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
 
-# reduce memory usage monitoring
-DDEFS += -DRAM_UNUSED_SIZE=100
-
-# Not use critical LED
-DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# assign critical LED to a non-existent pin if you do not have it on your board
+# good old PD14 is still the default value
+# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
