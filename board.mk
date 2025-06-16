e62c08413eaf70c1181fbefb4ba30fcfd41679e9
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2025-06-16 12:48:24 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,8 +1,4 @@
-BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
-    $(BOARD_DIR)/default_tune.cpp \
-
-
-BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
@@ -14,6 +10,4 @@ DDEFS += -DSTM32F407xx
 DDEFS += -DEFI_MAX_31855=TRUE
 DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
 # DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
