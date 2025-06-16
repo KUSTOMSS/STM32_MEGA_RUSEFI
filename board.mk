1c742f763fc86965643904b75b1db482eaafe810
KUSTOMSS <159179180+KUSTOMSS@users.noreply.github.com>
2025-06-16 12:08:47 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,20 +1,19 @@
-include $(BOARD_DIR)/firmware/firmware.mk
-
-
-BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+# List of all the board related files.
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
+# Override DEFAULT_ENGINE_TYPE
+DDEFS += -DSTM32F407xx
+DDEFS += -DFIRMWARE_ID=\"KRC_M52\"
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
+
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
-DDEFS += -DSTM32F407xx
-DDEFS += -DEFI_MAX_31855=TRUE
-DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
-
-# this would save some flash while being unable to update WBO controller firmware
-DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
+# Not use critical LED
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::Unassigned
 
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
