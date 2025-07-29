d897b2f51995e6c55cc8fac1c52a9b545b1f8137
GitHub git update Action <action@github.com>
2025-07-29 02:40:28 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_STM32_MEGA.h
+++ generated/controllers/generated/signature_STM32_MEGA.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 239477042
-#define TS_SIGNATURE "rusEFI main.2025.07.28.STM32_MEGA.239477042"
+#define SIGNATURE_HASH 2203887868
+#define TS_SIGNATURE "rusEFI main.2025.07.29.STM32_MEGA.2203887868"
